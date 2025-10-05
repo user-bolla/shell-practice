855abc8a32ebcd81cc0f453921cddec0504c197f
srimanth bolla <srimanthbolla9126@gmail.com>
2025-10-05 13:15:07 -0400

    hello world

---
+++ 12-installations.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+USERID = $(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo " ERROR:: Please run this script with root privelege"
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+    echo " ERROR:: Installing mysql is failed"
+else
+    echo " Installing mysql is success"
+fi
