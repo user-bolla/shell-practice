29f0e6f81721b868fc7479b3b5cc9bd431d2b7cc
srimanth bolla <srimanthbolla9126@gmail.com>
2025-10-05 13:23:04 -0400

    hello world

--- 12-installations.sh
+++ 12-installations.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-USERID= $(id -u)
+USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
-    echo " ERROR:: Please run this script with root privelege"
+    echo "ERROR:: Please run this script with root privelege"
     exit 1 # failure is other than 0
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
-    echo " ERROR:: Installing mysql is failed"
+    echo "ERROR:: Installing mysql is failed"
     exit 1 # failure is other than 0
 else
     echo " Installing mysql is success"
