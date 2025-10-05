d4fc89918c7e9e3cd5daed1272dc0210903e9d45
srimanth bolla <srimanthbolla9126@gmail.com>
2025-10-05 13:35:24 -0400

    hello world

---
+++ 13-installations.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
+fi
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "ERROR:: Installing $2 is failed"
+        exit 1 # failure is other than 0
+    else
+         echo " Installing $2 is success"  
+    fi
+}
+
+dnf install mysql -y
+
+VALIDATE $? "Mysql"
+
+dnf install nginx -y
+
+VALIDATE $? "nginx"
+
+dnf install mongodb-mongosh -y
+
+VALIDATE $? "mongosh
