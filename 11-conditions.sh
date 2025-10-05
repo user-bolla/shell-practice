34382fb307e6e6322bb253c86fc9c2de8b9a6ded
srimanth bolla <srimanthbolla9126@gmail.com>
2025-10-05 13:05:17 -0400

    hello world

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -3,7 +3,7 @@
 echo " please enter the number"
 read NUMBER
 
-if [ $(($NUMBER % 2)) -eq 0]; then
+if [ $(($NUMBER % 2)) -eq 0 ]; then
     echo " given number $NUMBER is Even number"
 else
     echo " Given number $NUMBER is odd number"
