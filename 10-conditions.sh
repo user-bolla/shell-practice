a7196f3eafc7b78a60dd71fa7d07a94f070ef3c7
srimanth bolla <srimanthbolla9126@gmail.com>
2025-10-05 12:57:07 -0400

    hello world

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -2,7 +2,7 @@
 
 NUMBER=$1
 
-if [ $NUMBER -lt 10 ]; then
+if [ {$NUMBER} -lt 10 ]; then
     echo "Given number $NUMBER is less than 10"
 elif [ $NUMBER -eq 10 ]; then
     echo "Given number $NUMBER is equal to 10"
