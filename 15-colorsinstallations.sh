5672591dcfd9b915a9bb6f0f089b3e25aef559d2
srimanth bolla <srimanthbolla9126@gmail.com>
2025-10-05 13:57:12 -0400

    hello world

---
+++ 15-colorsinstallations.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
+fi
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "Installing $2 is .. $R FAILED $N"
+        exit 1 # failure is other than 0
+    else
+         echo -e " Installing $2 is $G success $N"  
+    fi
+}
+
+dnf list installed mysql
+# Install if not found
+if [ $? -ne 0]; then
+
+    dnf install mysql -y
+    VALIDATE $? "Mysql"
+else
+    echo " Mysql already exist ... $G SKIPPING $N"
+fi
+dnf list installed nginx
+# Install if not found
+if [ $? -ne 0]; then
+
+    dnf install nginx -y
+    VALIDATE $? "nginx"
+else
+    echo -e " nginx already exist ... $G SKIPPING $N"
+fi
+
+dnf list installed python3
+# Install if not found
+if [ $? -ne 0]; then
+    dnf install python3 -y
+    VALIDATE $? "python3"
+else 
+    echo -e "Python3  already exist ... $G SKIPPING $N"
